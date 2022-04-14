e71364841c7772f04b3d2f6acd9425d44453fe88
SimpleBeat <simplebeat.katulin@gmail.com>
2022-04-14 14:48:44 +0300

    complete assignment

--- app.py
+++ app.py
@@ -3,6 +3,21 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route("/")
-def start():
+@app.route("/start")
+@app.route("/dictionary/<string:word>")
+def start(word=""):
 
-    return render_template('dictionary.html')
+    f = open("words.txt")
+    all_words = f.read().splitlines()
+    f.close()
+
+    number_of_words_left = 0
+
+    if word == "":
+        number_of_words_left = len(all_words)
+    else:
+        for w in all_words:
+            if w.startswith(word):
+                number_of_words_left += 1
+
+    return render_template('dictionary.html', number_of_words=number_of_words_left, word=word)
